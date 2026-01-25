d5d11fef8490544a64300434e7dfdcad50602806
John Clarence Palma <johnclarencepalma7@gmail.com>
2026-01-25 22:57:30 +0800

    update: line 105 -key , comments

--- src/index.js
+++ src/index.js
@@ -102,8 +102,9 @@ function SkillList() {
 
   return (
     <div className='skill-list' >
-      {defer ? (skillsArr.map((def) => <Skill skill={def.skill} color={def.color} levels={def.level} txtColor={def.textColor} />)) : <PopMessage />}
-      {/*we use the props in our array*/}
+      {defer ? (skillsArr.map((def) => <Skill skill={def.skill} color={def.color} levels={def.level} txtColor={def.textColor} key={def.skill} />)) : <PopMessage />}
+      {/*we check if the defer if it's true or false*/}
+      {/*then we check the sample data {skillsArr}*/}
     </div>
   )
 }
