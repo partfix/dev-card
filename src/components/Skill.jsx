e19eaaa9b6a77380981d8527ce402314b199a27e
John Clarence Palma <johnclarencepalma7@gmail.com>
2026-07-14 23:26:57 +0800

    feat: cleaning and understanding code

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -4,7 +4,6 @@ export function Skill({ skill, color, levels, txtColor }) {
 	return (
 		<div className='skill' style={{ backgroundColor: color, color: txtColor }}>
 			<span>{skill}</span>
-			{/*<span>{levels === 'Intermediate' && '👍🏻'}</span>*/}
 		</div>
 	);
 }
